85547f0150daf217909f1c7f2cf03e99241778cf
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-28 12:51:05 +0200

    adding ckeditor

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import TextareaAutosize from 'vue-textarea-autosize'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import CKEditor from '@ckeditor/ckeditor5-vue'
+
 require('./assets/sass/style.scss')
 
 Vue.config.productionTip = false
 Vue.use(TextareaAutosize)
+Vue.use(CKEditor)
 
 new Vue({
   router,
