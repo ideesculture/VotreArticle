4d15b2537e551ddd602e874293125a2c5847ac10
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-28 12:50:27 +0200

    improve design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="home container">
+  <div class="home container" style="padding-top: 20px;">
     <Preview msg=""></Preview>
-    <div id="nbcharacters" style="position:absolute;top:-60px;right:-30px;">{{nbcharacters}} caractères/<span style="margin-right:11px">{{maxcharacters}}</span>
-      <v-progress-circular size="60" color="#C44" width="10" rotate="-90" :value="percentagechars"></v-progress-circular>
+    <div id="nbcharacters" style="position:absolute;top:-100px;right:10px;width:270px;">
+      <div style="position:relative;top:60px;left:0;width:200px;">
+        {{nbcharacters}} caractères/<span style="margin-right:11px">{{maxcharacters}}</span>
+        <br/>
+        <small style="color:gray;">{{nbwords}} mots/{{maxwords}}</small>
+      </div>
+      <v-progress-circular size="60" :color="progressColor" width="10" rotate="-90" :value="percentagechars"></v-progress-circular>
     </div>
 
-    <textarea-autosize
-      placeholder="Le contenu de votre article"
-      ref="myTextarea"
-      v-model="article"
-      :min-height="30"
-      :max-height="350"
-      @blur.native="onBlurTextarea"
-    />
+    <p v-if="percentagechars > 100" class="is-danger has-text-left-desktop"
+       style="padding:10px 20px;margin-bottom:10px;background-color:#d3394c;border-radius: 6px;color:white">
+      Votre texte est long, merci de le raccourcir.
+    </p>
+    <ckeditor :editor="editor" v-model="trimmedArticle" :config="editorConfig"></ckeditor>
     <div style="margin-top: 20px;" class="columns">
       <div class="column grandeimagecol">
-        <div class="helper"></div>
         <div class="drop display-inline align-center" @dragover.prevent @drop="onDrop">
-          <div class="helper"></div>
-          <div class="helper"></div>
           <label v-if="!image" class="btn display-inline">
             CHOISISSEZ OU GLISSEZ L'IMAGE PRINCIPALE
             <input type="file" name="image" @change="onChange">
@@ -32,63 +31,73 @@
         </div>
       </div>
       <div class="column">
-        <div class="helper"></div>
         <div class="drop display-inline align-center" @dragover.prevent @drop="onDrop">
-          <div class="helper"></div>
-          <div class="helper"></div>
-          <label v-if="!image" class="btn display-inline">
+          <label v-if="!image2" class="btn display-inline">
             IMAGE 2
-            <input type="file" name="image" @change="onChange">
+            <input type="file" name="image2" @change="onChange2">
           </label>
           <div class="hidden display-inline align-center" v-else v-bind:class="{ 'image': true }">
-            <img :src="image" alt="" class="img" />
+            <img :src="image2" alt="" class="img" />
             <br>
             <br>
-            <button class="btn" @click="removeFile">ENLEVER</button>
+            <button class="btn" @click="removeFile2">ENLEVER</button>
           </div>
         </div>
-        <div class="helper"></div>
         <div class="drop display-inline align-center" @dragover.prevent @drop="onDrop">
-          <div class="helper"></div>
-          <div class="helper"></div>
-          <label v-if="!image" class="btn display-inline">
+          <label v-if="!image3" class="btn display-inline">
             IMAGE 3
-            <input type="file" name="image" @change="onChange">
+            <input type="file" name="image3" @change="onChange3">
           </label>
           <div class="hidden display-inline align-center" v-else v-bind:class="{ 'image': true }">
-            <img :src="image" alt="" class="img" />
+            <img :src="image3" alt="" class="img" />
             <br>
             <br>
-            <button class="btn" @click="removeFile">ENLEVER</button>
+            <button class="btn" @click="removeFile3">ENLEVER</button>
           </div>
         </div>
       </div>
     </div>
+    <button class="button is-info">Envoyer</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Preview from '@/components/Preview'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 export default {
   name: 'Home',
   data: function () {
     return {
       image: '',
+      image2: '',
+      image3: '',
       maxcharacters: 1700,
       maxwords: 250,
-      article: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae urna massa. Morbi vehicula, dolor vitae ultricies luctus, nisl ante maximus enim, vitae posuere purus nulla a risus. Ut volutpat nisl et aliquet consequat. Phasellus ut consequat elit. Suspendisse a dolor sodales, congue felis ac, iaculis nisi. Cras non purus id sapien posuere mattis in laoreet augue. Maecenas eu velit convallis, egestas purus nec, suscipit elit. Mauris sed auctor libero. Pellentesque eget maximus velit.\n' +
-        '\n' +
-        'Etiam ultrices ac eros quis imperdiet. Nullam semper, leo ac venenatis tincidunt, enim nisl tincidunt sem, nec pharetra mi risus vitae augue. Aliquam molestie urna venenatis est scelerisque interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla finibus, diam vel auctor dapibus, ante felis semper lectus, ac elementum ex nisl eu eros. Duis id arcu nisi. Mauris quam odio, fringilla et porttitor eget, commodo elementum lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec posuere nisl quis odio finibus facilisis. Mauris et feugiat nibh, condimentum mollis elit. Mauris id massa nisl. Aliquam erat volutpat. Aliquam a posuere tellus.\n' +
-        '\n' +
-        'Donec ut congue ipsum. '
+      article: 'Saisissez ou copiez-coller dans cette zone le texte de votre article. ',
+      editor: ClassicEditor,
+      editorData: '<p>Content of the editor.</p>',
+      editorConfig: {
+        toolbar: ['bold', 'italic', 'bulletedList'],
+        extraPlugins: ''
+        // The configuration of the editor.
+      }
     }
   },
   components: {
     Preview
   },
   computed: {
+    trimmedArticle:
+    {
+      get () {
+        return this.article.slice(0, Math.round(this.maxcharacters * 1.03))
+      },
+      set (value) {
+        this.article = value.slice(0, Math.round(this.maxcharacters * 1.03))
+      }
+    },
     nbcharacters: function () {
       return this.article.length
     },
@@ -97,6 +106,21 @@ export default {
     },
     nbwords: function () {
       return (this.article.split(' ').length - 1)
+    },
+    progressColor: function () {
+      if (this.percentagechars < 40) {
+        return '#ffffff'
+      } else if (this.percentagechars < 90) {
+        return '#55bb55'
+      } else if (this.percentagechars <= 100) {
+        return '#ff7f00'
+      } else {
+        return '#ff3300'
+      }
+    },
+    actionUpdate: function () {
+      console.log('update')
+      return true
     }
   },
   methods: {
@@ -109,6 +133,14 @@ export default {
     onChange (e) {
       var files = e.target.files
       this.createFile(files[0])
+    },
+    onChange2 (e) {
+      var files = e.target.files
+      this.createFile2(files[0])
+    },
+    onChange3 (e) {
+      var files = e.target.files
+      this.createFile3(files[0])
     },
     createFile (file) {
       if (!file.type.match('image.*')) {
@@ -123,8 +155,40 @@ export default {
       }
       reader.readAsDataURL(file)
     },
+    createFile2 (file) {
+      if (!file.type.match('image.*')) {
+        alert('Select an image')
+        return
+      }
+      var reader = new FileReader()
+      var vm = this
+
+      reader.onload = function (e) {
+        vm.image2 = e.target.result
+      }
+      reader.readAsDataURL(file)
+    },
+    createFile3 (file) {
+      if (!file.type.match('image.*')) {
+        alert('Select an image')
+        return
+      }
+      var reader = new FileReader()
+      var vm = this
+
+      reader.onload = function (e) {
+        vm.image3 = e.target.result
+      }
+      reader.readAsDataURL(file)
+    },
     removeFile () {
       this.image = ''
+    },
+    removeFile2 () {
+      this.image2 = ''
+    },
+    removeFile3 () {
+      this.image3 = ''
     }
   }
 }
