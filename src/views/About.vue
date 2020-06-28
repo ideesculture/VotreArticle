7e0ae1936328d8ec0f888dc98718b69f0b45e820
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-28 09:57:30 +0200

    à propos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>A propos</h1>
+    <h1 class="title">A propos</h1>
+    <div class="container">
+      <div style="text-align: left;">
+        <p>Cette application a été développée sous <a href="https://fr.vuejs.org/index.html">Vue.JS</a>.</p>
+        <p>Elle vise à permettre à des rédacteurs externes de rédiger ou de copier-coller leurs articles pour une revue interne papier, un prédépôt avant du web, ...</p>
+        <hr/>
+        <p><small>Gautier Michelin, 2020</small></p>
+      </div>
+    </div>
   </div>
 </template>
